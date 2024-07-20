c107d11219f0b545416004e4ff352bb09b390fb6
Sumitminhas09 <sumitminhas789@gmail.com>
2024-07-20 22:10:55 +0530

    origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://meek-valkyrie-ce1e61.netlify.app/' ,'https://meek-valkyrie-ce1e61.netlify.app'
-
+    'https://meek-valkyrie-ce1e61.netlify.app'
 ];
 
 module.exports = allowedOrigins;
